0567b66445cd5efcb17faaca90f24d829829bcee
Achal Dave <achalddave@live.com>
2019-07-27 09:45:10 -0400

    Add missing files for generating thumbnails in JS

---
+++ labeler/labelers/single_video_with_serverside_thumbnails.py
@@ -0,0 +1,117 @@
+import collections
+import csv
+import tempfile
+import threading
+from functools import lru_cache
+from pathlib import Path
+from typing import NamedTuple, Optional
+
+import flask
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from PIL import Image
+
+from labeler.labelers.single_image import SingleFileLabeler
+from labeler.label_stores.json_label_store import JsonLabelStore
+from labeler.utils.fs import VIDEO_EXTENSIONS
+from labeler.utils import video as video_utils
+
+
+class SingleVideoWithThumbnailsLabeler(SingleFileLabeler):
+    def __init__(self,
+                 root,
+                 labels_csv,
+                 output_dir,
+                 num_thumbnails=10,
+                 thumb_duration=0,  # Set to 0 to generate image thumbnails
+                 extensions=VIDEO_EXTENSIONS):
+        super().__init__(root, extensions, labels_csv, output_dir)
+        self.num_thumbnails = num_thumbnails
+        self.thumb_duration = thumb_duration
+        self.thumbnail_dir = self.output_dir / 'thumbnails'
+        self.thumbnail_dir.mkdir(exist_ok=True, parents=True)
+        # ffmpeg starts failing if you have too many parallel instances of it
+        # running (through moviepy).
+        self._thumbnail_semaphore = threading.Semaphore(8)
+
+    def public_directories(self):
+        dirs = {
+            'thumb': str(self.thumbnail_dir)
+        }
+        dirs.update(super().public_directories())
+        return dirs
+
+    def thumbnail_to_url(self, key):
+        relative = str(Path(key).relative_to(self.thumbnail_dir))
+        return f'/file/thumb/{relative}'
+
+    def get_thumbnail(self, video, index):
+        with self._thumbnail_semaphore:
+            return self._get_thumbnail_unsafe(video, index)
+
+    def _get_thumbnail_unsafe(self, video, index):
+        video = self.url_to_key(video)
+        relative = video.relative_to(self.root)
+        thumbnail_dir = self.thumbnail_dir / relative
+        thumbnail_dir.mkdir(exist_ok=True, parents=True)
+
+        assert video.exists(), f'{video} does not exist.'
+        clip = VideoFileClip(str(video))
+        num_frames = video_utils.num_frames(video)
+        num_thumbnails = min(self.num_thumbnails, num_frames)
+
+        thumbnail_frames = [
+            round(i * num_frames / (num_thumbnails + 2))
+            for i in range(num_thumbnails + 2)
+        ]
+        # Remove first and last frame.
+        thumbnail_frames = thumbnail_frames[1:-1]
+        frame = thumbnail_frames[index]
+
+        duration = self.thumb_duration
+        thumb_type = "mp4" if duration > 0 else "jpg"
+        output_thumbnail = thumbnail_dir / (f'frame-{frame:04d}_{duration}s.' +
+                                            thumb_type)
+        if not output_thumbnail.exists():
+            t = frame / clip.fps
+            if duration > 0:
+                subclip = clip.subclip(t - duration / 2, t + duration / 2)
+                subclip.write_videofile(str(output_thumbnail), audio=False)
+            else:
+                Image.fromarray(clip.get_frame(t)).save(output_thumbnail)
+        print(f'Finished generating thumbnail for {video} @ {index}')
+        return self.thumbnail_to_url(output_thumbnail)
+
+    def api(self, api_request):
+        try:
+            request, params = api_request.split('/', 1)
+        except ValueError:
+            flask.abort(404)
+
+        if request == 'thumbnail':
+            thumbnail_index = int(params.split('/')[-1])
+            video = '/'.join(params.split('/')[:-1])
+            return flask.redirect(self.get_thumbnail(video, thumbnail_index))
+        else:
+            flask.abort(404)
+
+    def key_to_thumb_urls(self, key):
+        video = self.key_to_url(key)
+        return [
+            f'/api/thumbnail/{video}/{i}' for i in range(self.num_thumbnails)
+        ]
+
+    def index(self):
+        video_keys = self.label_store.get_unlabeled(self.num_items)
+        total_videos = self.label_store.num_total()
+        num_complete = self.label_store.num_completed()
+        percent_complete = 100 * num_complete / max(total_videos, 1e-9)
+        return flask.render_template(
+            'label_video_with_serverside_thumbnails.html',
+            num_left_videos=total_videos - num_complete,
+            num_total_videos=total_videos,
+            percent_complete='%.2f' % percent_complete,
+            videos_to_label=[(key, self.key_to_url(key),
+                              self.key_to_thumb_urls(key), None)
+                             for key in video_keys],
+            image_thumbnails=self.thumb_duration == 0,
+            labels=[x for x in self.labels])
---
+++ labeler/static/video_with_thumbnails.js
@@ -0,0 +1,172 @@
+// Modified from VGG VIA: https://gitlab.com/vgg/via/
+class SingleVideoThumbnailCreator {
+  constructor(videoSrc) {
+    this.videoSrc = videoSrc;
+    this.fwidth = 160;
+    this.thumbnails = {};
+
+    // state
+    this.is_thumbnail_read_ongoing = false;
+    this.thumbnail_time = 0;
+    this.thumbnail_canvas = document.createElement("canvas");
+  }
+
+  load() {
+    return new Promise(
+      function(ok_callback, err_callback) {
+        this._load_video().then(
+          function() {
+            this.video.currentTime = 0.0;
+            ok_callback();
+          }.bind(this),
+          function(load_err) {
+            console.log(load_err);
+            err_callback();
+          }.bind(this)
+        );
+      }.bind(this)
+    );
+  }
+
+  _load_video() {
+    return new Promise(
+      function(ok_callback, err_callback) {
+        this.video = document.createElement("video");
+        this.video.setAttribute("src", this.videoSrc);
+        this.video.setAttribute("preload", "auto");
+
+        this.video.addEventListener(
+          "loadeddata",
+          function() {
+            let aspect_ratio = this.video.videoHeight / this.video.videoWidth;
+            this.fheight = Math.floor(this.fwidth * aspect_ratio);
+            this.thumbnail_canvas.width = this.fwidth;
+            this.thumbnail_canvas.height = this.fheight;
+            this.thumbnail_context = this.thumbnail_canvas.getContext("2d", {
+              alpha: false
+            });
+            ok_callback();
+          }.bind(this)
+        );
+
+        this.video.addEventListener(
+          "error",
+          function() {
+            err_callback("error");
+          }.bind(this)
+        );
+        this.video.addEventListener(
+          "abort",
+          function() {
+            err_callback("abort");
+          }.bind(this)
+        );
+
+        this.video.addEventListener("seeked", this._on_seeked.bind(this));
+      }.bind(this)
+    );
+  }
+
+  get_thumbnail(time_float) {
+    this.is_thumbnail_read_ongoing = true;
+    this.thumbnail_time = parseInt(time_float);
+    this.video.currentTime = this.thumbnail_time;
+    return new Promise((ok_callback) => {
+      this._thumbnail_generated_callback = ok_callback;
+    });
+  }
+
+  _on_seeked() {
+    if (this.is_thumbnail_read_ongoing && this.thumbnail_context) {
+      this.is_thumbnail_read_ongoing = false;
+      this.thumbnail_context.drawImage(
+        this.video,
+        0,
+        0,
+        this.video.videoWidth,
+        this.video.videoHeight,
+        0,
+        0,
+        this.fwidth,
+        this.fheight
+      );
+      this._thumbnail_generated_callback();
+    }
+  }
+}
+
+class ThumbnailCreator {
+  constructor() {
+    // Contains container ids for which thumbnails have been generated.
+    this.thumbnailsGenerated = {};
+  }
+
+  createContainerThumbnails(labelContainer) {
+    let containerId = labelContainer.attr("id");
+
+    let thumbnailContainer = labelContainer.find(
+      ".to-label-thumbnails-container"
+    );
+
+    if (this.thumbnailsGenerated.hasOwnProperty(containerId)) {
+      return;
+    }
+
+    let video = labelContainer.find("video.to-label");
+    let numThumbnails = parseInt(thumbnailContainer.attr("num-thumbnails"));
+
+    let creator = new SingleVideoThumbnailCreator(video.attr("src"));
+
+    return new Promise((ok_callback, err_callback) => {
+      creator.load().then(() => {
+        let duration = creator.video.duration;
+        let promise = Promise.resolve();
+        for (let i = 1; i < numThumbnails + 1; ++i) {
+          let thumbnailTime = (duration * i) / numThumbnails;
+          // Next promise will be resolved after this thumbnail is received.
+          promise = promise.then(() => {
+            return creator.get_thumbnail(thumbnailTime);
+          });
+
+          promise = promise.then(() => {
+            let previewCanvas = creator.thumbnail_canvas;
+            let thumbnailCanvas = $("<canvas>")[0];
+            thumbnailCanvas.width = previewCanvas.width;
+            thumbnailCanvas.height = previewCanvas.height;
+            thumbnailCanvas.getContext("2d").drawImage(previewCanvas, 0, 0);
+            let canvasContainer = $(
+              "<div class='thumbnail-canvas-container'></div>"
+            );
+            canvasContainer.append(thumbnailCanvas);
+            thumbnailContainer.append(canvasContainer);
+          });
+        }
+        this.thumbnailsGenerated[containerId] = true;
+        promise.then(() => {
+          ok_callback();
+        }).catch(err_callback);
+      });
+    })
+  }
+
+  showThumbnails() {
+    // Hide all other thumbnails
+    $(".to-label-thumbnails-container").hide();
+    $(".active").find(".to-label-thumbnails-container").show();
+  }
+}
+
+let thumbnailer = new ThumbnailCreator();
+$(function() {
+  let thumbnailPromise = Promise.resolve();
+  $(".data-label-container").each(function() {
+    thumbnailPromise = thumbnailPromise.then(() => {
+      thumbnailer.createContainerThumbnails($(this));
+    });
+  });
+});
+
+window.addEventListener(
+  "activeContainerUpdated",
+  thumbnailer.showThumbnails.bind(thumbnailer)
+);
