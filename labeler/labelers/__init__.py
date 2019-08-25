b02762061bcf80fee4baf427c5e17271f4f4664d
Achal Dave <achalddave@live.com>
2019-08-25 21:29:46 +0000

    Register SingleImageWithCaptionsLabeler

--- labeler/labelers/__init__.py
+++ labeler/labelers/__init__.py
@@ -1,9 +1,11 @@
 from .single_image import SingleImageLabeler
+from .single_image_with_captions import SingleImageWithCaptionsLabeler
 from .single_video import SingleVideoLabeler
 from .single_video_with_thumbnails import SingleVideoWithThumbnailsLabeler
 
 labeler_dict = {
     'SingleImageLabeler': SingleImageLabeler,
     'SingleVideoLabeler': SingleVideoLabeler,
-    'SingleVideoWithThumbnailsLabeler': SingleVideoWithThumbnailsLabeler
+    'SingleVideoWithThumbnailsLabeler': SingleVideoWithThumbnailsLabeler,
+    'SingleImageWithCaptionsLabeler': SingleImageWithCaptionsLabeler
 }
